b7c871eb143bfa22d6a5c4816464d0dda147b0d7
Jake Cattrall <krazyjakee@gmail.com>
2013-04-09 09:11:00 +0100

    coffeescript is good :)

--- src/js/character.jquest.js
+++ src/js/character.jquest.js
@@ -1,119 +1,146 @@
-var Character = {
-	
-	settings: {
-		fileExtension: 'json',
-		spriteDirectory: ''
-	},
-	sprites: [],
-	characters: [],
-	playerSprite: false,
-	playerMoving: false,
-	loadSprite: function(charSource, callback){
-		var spritefile = Character.settings.spriteDirectory+charSource+'.'+Character.settings.fileExtension;
-		$.getJSON(spritefile, function(json){
-			Character.sprites[json.id] = json;
-			if(callback){
-				callback(json.id);
-			}
-		});
-	},
-	placeCharacter: function(id, sprite, location){
-		var loc = Map.tileIdPosition(location);
-		$('#layer'+Map.playerLayer).append('<div class="character" id="character'+id+'"></div>');
-		
-		var charid = '#character'+id;
-		$(charid).width(Character.sprites[sprite].cellWidth).height(Character.sprites[sprite].cellHeight).css('background-image','url('+Character.sprites[sprite].sprite+')').css('background-position','-'+Character.sprites[sprite].cellWidth+'px 0px').css('left',loc[0]).css('top',loc[1]);
-	},
-	loadPlayer: function(sprite){
-		Character.playerSprite = sprite;
-		
-		// ID 0 is always the player character.
-		Character.characters[0] = { sprite: sprite, timer: [] };
-		Character.placeCharacter(0, sprite, Map.playerTile);
-	},
-	playerMove: function(paths){
-		
-		/*
-			If the character is already moving at the end location is redefined, it's best to remove the first
-			of the next path as animation becomes more fluid.
-		*/
-		if(Character.playerMoving){
-			paths = paths.slice(1);
-			Character.stopAnimation(0);
-			for(var i = 0; i <= Character.characters[0].timer.length; i++){
-				clearTimeout(Character.characters[0].timer[i]);
-			}
-		}
-		$('#character0').clearQueue().stop();
-		
-		Character.playerMoving = true;
-		if(paths[0]){
-			Character.animate(0,paths[0].direction);
-		}
-		$(paths).each(function(index,path){
-			var loc = Map.tileIdPosition([path.x,path.y]);
-			if(path.direction){
-				Character.characters[0].timer[index] = setTimeout(function(){ Character.animate(0,path.direction); },index*310);
-			}
-			$('#character0').animate({
-				left: loc[0],
-				top: loc[1]
-			},300,'linear',function(){
-				if(index == paths.length-1){
-					Character.playerMoving = false;
-					Character.stopAnimation(0);
-				}
-				Map.playerTile = Map.tileIdConvert([path.x+1,path.y]);
-			});
-		});
-		
-	},
-	animate: function(id,direction){
-		Character.stopAnimation(id);
-		
-		Character.characters[id].animation = {timer: setInterval(function(){
-			if(Character.characters[id].animation.step > 5){
-				Character.characters[id].animation.step = 0;
-			}
-			var y = 0;
-			switch(direction){
-				case "n":
-					var y = 3 * Character.sprites[id].cellHeight;
-					break;
-				case "e":
-					var y = 1 * Character.sprites[id].cellHeight;
-					break;
-				case "s":
-					var y = 0 * Character.sprites[id].cellHeight;
-					break;
-				case "w":
-					var y = 2 * Character.sprites[id].cellHeight;
-					break;
-				case "ne":
-					var y = 6 * Character.sprites[id].cellHeight;
-					break;
-				case "nw":
-					var y = 7 * Character.sprites[id].cellHeight;
-					break;
-				case "se":
-					var y = 5 * Character.sprites[id].cellHeight;
-					break;
-				case "sw":
-					var y = 4 * Character.sprites[id].cellHeight;
-					break;				
-			}
-			var x = Character.characters[id].animation.step * Character.sprites[id].cellWidth;
-			$('#character'+id).css('background-position','-'+x+'px -'+y+'px');
-			Character.characters[id].animation.step++
-		},200), step: 0, y: 0 }
-	},
-	stopAnimation: function(id){
-		if(Character.characters[id].animation){
-			clearInterval(Character.characters[id].animation.timer);
-			var bgpos = $('#character'+id).css('background-position').split(' ');
-			bgpos[0] = '-'+Character.sprites[id].cellWidth+'px';
-			$('#character'+id).css('background-position',bgpos.join(' '));
-		}
-	}
-	
-}
+(function() {
+
+  window.Character = (function() {
+
+    function Character() {}
+
+    Character.settings = {
+      fileExtension: 'json',
+      spriteDirectory: ''
+    };
+
+    Character.sprites = [];
+
+    Character.characters = [];
+
+    Character.playerSprite = false;
+
+    Character.playerMoving = false;
+
+    Character.loadSprite = function(charSource, callback) {
+      var spritefile;
+      spritefile = "" + (this.settings.spriteDirectory + charSource) + "." + this.settings.fileExtension;
+      return $.getJSON(spritefile, function(json) {
+        window.Character.sprites[json.id] = json;
+        if (callback) {
+          return callback(json.id);
+        }
+      });
+    };
+
+    Character.placeCharacter = function(id, sprite, location) {
+      var charid, loc;
+      loc = window.Map.tileIdPosition(location);
+      $("#layer" + Map.playerLayer).append("<div class=\"character\" id=\"character" + id + "\" />");
+      charid = "#character" + id;
+      return $(charid).width(this.sprites[sprite].cellWidth).height(this.sprites[sprite].cellHeight).css("background-image", "url(" + this.sprites[sprite].sprite + ")").css("background-position", "-" + this.sprites[sprite].cellWidth + "px 0px").css("left", loc[0]).css("top", loc[1]);
+    };
+
+    Character.loadPlayer = function(sprite) {
+      window.Character.playerSprite = sprite;
+      window.Character.characters[0] = {
+        sprite: sprite,
+        timer: []
+      };
+      return window.Character.placeCharacter(0, sprite, window.Map.playerTile);
+    };
+
+    Character.playerMove = function(paths) {
+      /*
+          If the character is already moving at the end location is redefined, it's best to remove the first
+          of the next path as animation becomes more fluid.
+      */
+
+      var index, _i, _ref;
+      if (this.playerMoving) {
+        paths = paths.slice(1);
+        this.stopAnimation(0);
+        for (index = _i = 0, _ref = this.characters[0].timer.length; 0 <= _ref ? _i < _ref : _i > _ref; index = 0 <= _ref ? ++_i : --_i) {
+          clearTimeout(this.characters[0].timer[index]);
+        }
+      }
+      $('#character0').clearQueue().stop();
+      this.playerMoving = true;
+      if (paths[0]) {
+        this.animate(0, paths[0].direction);
+      }
+      return $.each(paths, function(index, path) {
+        var loc;
+        loc = Map.tileIdPosition([path.x, path.y]);
+        if (path.direction) {
+          window.Character.characters[0].timer[index] = setTimeout(function() {
+            return window.Character.animate(0, path.direction);
+          }, index * 310);
+        }
+        return $('#character0').animate({
+          left: loc[0],
+          top: loc[1]
+        }, 300, 'linear', function() {
+          if (index === paths.length - 1) {
+            window.Character.playerMoving = false;
+            window.Character.stopAnimation(0);
+          }
+          return Map.playerTile = Map.tileIdConvert([path.x + 1, path.y]);
+        });
+      });
+    };
+
+    Character.animate = function(id, direction) {
+      window.Character.stopAnimation(id);
+      return window.Character.characters[id].animation = {
+        timer: setInterval(function() {
+          var x, y;
+          if (window.Character.characters[id].animation.step > 5) {
+            window.Character.characters[id].animation.step = 0;
+          }
+          y = 0;
+          switch (direction) {
+            case "n":
+              y = 3;
+              break;
+            case "e":
+              y = 1;
+              break;
+            case "s":
+              y = 0;
+              break;
+            case "w":
+              y = 2;
+              break;
+            case "ne":
+              y = 6;
+              break;
+            case "nw":
+              y = 7;
+              break;
+            case "se":
+              y = 5;
+              break;
+            case "sw":
+              y = 4;
+          }
+          y *= window.Character.sprites[id].cellHeight;
+          x = window.Character.characters[id].animation.step * window.Character.sprites[id].cellWidth;
+          $("#character" + id).css("background-position", "-" + x + "px -" + y + "px");
+          return window.Character.characters[id].animation.step++;
+        }, 200),
+        step: 0,
+        y: 0
+      };
+    };
+
+    Character.stopAnimation = function(id) {
+      var bgpos;
+      if (window.Character.characters[id].animation) {
+        clearInterval(window.Character.characters[id].animation.timer);
+        bgpos = $("#character" + id).css('background-position').split(' ');
+        bgpos[0] = "-" + this.sprites[id].cellWidth + "px";
+        return $("#character" + id).css('background-position', bgpos.join(' '));
+      }
+    };
+
+    return Character;
+
+  })();
+
+}).call(this);
--- src/js/map.jquest.js
+++ src/js/map.jquest.js
@@ -1,211 +1,244 @@
-var Map = {
-	settings: {
-		fileExtension: 'json',
-		mapDirectory: ''
-	},
-	mapData: false,
-	renderedTiles: [],
-	tileProperties: [],
-	playerTile: false,
-	showBlocked: false,
-	showPaths: false,
-	playerLayer: 0,
-	isMoving: false,
-	drawMap: function(targetElem){
-		$.each(Map.mapData.layers, function(index, layer){
-			if(layer.type == "tilelayer"){
-				$(targetElem).append('<div class="layer" id="layer'+index+'"></div>');
-					
-				for(var i = 0; i < layer.data.length; i++){
-					$('#layer'+index).append('<div id="tile'+index+'-'+i+'" class="tile"></div>');
-					Map.drawTile(index, layer.data[i], i);
-				}
-				
-				$('#layer'+index).css('width',Map.mapData.tilewidth * Map.mapData.width + 'px');
-				$('#layer'+index).css('height',Map.mapData.tileheight * Map.mapData.height + 'px');
-				$('.tile').width(Map.mapData.tilewidth);
-				$('.tile').height(Map.mapData.tileheight);
-				$(targetElem).parent().addClass('viewport');
-				
-				$('.layer:last').find('.tile').click(Map.tileClick);
+(function() {
 
-				$('#maploading').remove();
-			}else if(layer.type == "objectgroup"){
-			
-				if(layer.name == "Player"){
-					Map.playerLayer = index;
-				}
-				
-				$(targetElem).append('<div class="layer" id="layer'+index+'"></div>');
-			}
-		});
-	},
-	loadMap: function(targetElem, mapSource, callback){
-		if(typeof mapSource == 'object'){
-			Map.mapData = mapSource;
-			Map.drawMap(targetElem);
-			if(callback){
-				callback();
-			}
-		}else if(typeof mapSource == 'string'){
-			$.getJSON(Map.settings.mapDirectory+mapSource+'.'+Map.settings.fileExtension, function(json){
-				Map.mapData = json;
-				Map.drawMap(targetElem);
-				if(callback){
-					callback();
-				}
-			});
-		}
-	},
-	drawTile: function(layer, srcTile, targetTile){
-		var target = $('#tile'+layer+'-'+targetTile);
-			
-		var setData = false;
-		$.each(Map.mapData.tilesets, function(index, tileset){
-			if(srcTile >= tileset.firstgid){
-				setData = tileset;
-			}else{
-				return false;
-			}
-		});
-		
-		var setWidth = Math.floor(setData.imagewidth / setData.tilewidth)-1;
-		var setHeight = Math.floor(setData.imageheight / setData.tileheight)-1;
-		
-		var heightCount = 0;
-		var widthCount = 0;
-		for(var i = 1; i < srcTile; i++){
-			if(widthCount < setWidth){
-				widthCount++;
-			}else{
-				widthCount = 0;
-				heightCount++;
-			}
-		}
-		
-		var offset = {
-			x: (widthCount * setData.tilewidth) + (setData.spacing * widthCount) + setData.margin,
-			y: (heightCount * setData.tileheight) + (setData.spacing * heightCount) + setData.margin
-		}
-		
-		if(setData.image){
-			Map.renderedTiles[srcTile] = '#tile'+layer+'-'+targetTile;
-			var property = Map.tileProperty(srcTile);
-			if(property != false){
-				Map.tileProperties[targetTile] = property;
-			}
-			$(target).css('background-image','url('+setData.image+')');
-			$(target).css('background-position','-'+offset.x+'px -'+offset.y+'px');
-		}
-	},
-	setFocus: function(tileId, duration){
-		var loc = Map.tileIdConvert(tileId);
-		var curloc = $('.layer:first').position();
-		var maxwidth = (Map.mapData.width*32) - $('.viewport').width();
-		var maxheight = (Map.mapData.height*32) - $('.viewport').height();
-		loc[0] = (loc[0]*32) - ($('.viewport').width()/2);
-		loc[1] = (loc[1]*32) - ($('.viewport').height()/2);
-		
-		// Set view limits
-		if(loc[1] < 0){loc[1]=0;}
-		if(loc[0] < 0){loc[0]=0;}
-		if(loc[1] > maxheight){loc[1]=maxheight;}
-		if(loc[0] > maxwidth){loc[0]=maxwidth;}
-		
-		Map.isMoving = true;
-		
-		$('.layer-container').stop();
-		$('.layer-container').animate({
-	        top: "-"+loc[1]+"px",
-			left: "-"+loc[0]+"px"
-	    }, duration, function(){
-	    	Map.isMoving = false;
-	    });
-	},
-	tileIdConvert: function(tileInput){
-		if(typeof tileInput == 'object'){
-			var tileId = tileInput[1] * Map.mapData.width-1;
-			tileId += tileInput[0];
-			return tileId;
-		}else{
-			var y = 0;
-			var x = 0;
-			var width = Map.mapData.width-1;
-			
-			for(var i = 0; i < tileInput; i++){
-				if(x < width){
-					x++;
-				}else{
-					x = 0;
-					y++;
-				}
-			}
-			return [x, y];
-		}
-	},
-	tileIdPosition: function(tileInput){
-		if(typeof tileInput != 'object'){
-			tileInput = Map.tileIdConvert(tileInput);
-		}
-		return [(tileInput[0]-1)*Map.mapData.tilewidth,tileInput[1]*Map.mapData.tileheight];
-	},
-	tileProperty: function(tileId){
-		tileId = tileId - 1;
-		var property = false;
-		$.each(Map.mapData.tilesets, function(index, data){
-			if(data.tileproperties != null && data.tileproperties[tileId] != null){
-				property = data.tileproperties[tileId].property;
-			}
-		});
-		return property;
-	},
-	tileClick: function(e){
-		var tileId = $(this).attr('id');
-		tileId = tileId.substr(tileId.lastIndexOf('-')+1);
-		tileId++;
-		if(e.button == 2){
-			alert()
-		}
-		else{
-			var paths = Map.makePath(tileId);
-			if(paths.length){
-				if(Map.showPaths){
-					$('#layer'+(Map.playerLayer-1)+' div').css('background-color','transparent');
-					$.each(paths, function(index, path){
-						var tileId = Map.tileIdConvert([path.x,path.y]);
-						$('#tile'+(Map.playerLayer-1)+'-'+tileId).css('background-color','red');
-					});
-				}
-				Map.setFocus(tileId, (paths.length * 500));
-				Map.playerTile = tileId;
-				
-				Character.playerMove(paths);
-			}
-		}
-	},
-	makePath: function(toTileId){
-		var totalMapSize = Map.mapData.width * Map.mapData.height;
-		var toTileLoc = Map.tileIdConvert(toTileId);
-		var fromTileLoc = Map.tileIdConvert(Map.playerTile);
-		var board = [];
-		
-		for(var y = 0; y < Map.mapData.height-1; y++)
-		{
-			board[y] = [];
-			for (var x = 0; x < Map.mapData.width-1; x++){
-				var tile = Map.tileIdConvert([x,y]);
-				var prop = Map.tileProperties[tile];
-				if(prop == 'block'){
-					board[y][x] = 1;
-					if(Map.showBlocked){
-						var tileidc = this.tileIdConvert([x,y]);
-						$('#tile0-'+tileidc).css('background','red');
-					}
-				}else{
-					board[y][x] = 0;
-				}
-			}
-		}
-		return AStar(board, fromTileLoc, toTileLoc, 'Diagonal');
-	}
-}
+  window.Map = (function() {
+
+    function Map() {}
+
+    Map.settings = {
+      fileExtension: 'json',
+      mapDirectory: ''
+    };
+
+    Map.mapData = false;
+
+    Map.renderedTiles = [];
+
+    Map.tileProperties = [];
+
+    Map.playerTile = false;
+
+    Map.showBlocked = false;
+
+    Map.showPaths = false;
+
+    Map.playerLayer = 0;
+
+    Map.isMoving = false;
+
+    Map.drawMap = function(targetElem) {
+      var currentLayer, i, index, layer, tile, _i, _j, _len, _len1, _ref, _ref1, _results;
+      _ref = this.mapData.layers;
+      _results = [];
+      for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
+        layer = _ref[index];
+        if (layer.type === "tilelayer") {
+          $(targetElem).append("<div class=\"layer\" id=\"layer" + index + "\" />");
+          currentLayer = $("#layer" + index);
+          _ref1 = layer.data;
+          for (i = _j = 0, _len1 = _ref1.length; _j < _len1; i = ++_j) {
+            tile = _ref1[i];
+            $(currentLayer).append("<div id=\"tile" + index + "-" + i + "\" class=\"tile\" />");
+            this.drawTile(index, tile, i);
+          }
+          $(currentLayer).css('width', this.mapData.tilewidth * this.mapData.width + 'px').css('height', this.mapData.tileheight * this.mapData.height + 'px');
+          $('.tile').width(this.mapData.tilewidth).height(this.mapData.tileheight);
+          $('.layer:last').find('.tile').click(this.tileClick);
+          _results.push($('#maploading').remove());
+        } else if (layer.type === "objectgroup") {
+          if (layer.name === "Player") {
+            this.playerLayer = index;
+          }
+          _results.push($(targetElem).append("<div class=\"layer\" id=\"layer" + index + "\" />"));
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
+    };
+
+    Map.loadMap = function(targetElem, mapSource, callback) {
+      if (typeof mapSource === 'object') {
+        this.mapData = mapSource;
+        this.drawMap(targetElem);
+        if (callback) {
+          return callback();
+        }
+      } else if (typeof mapSource === 'string') {
+        return $.getJSON(this.settings.mapDirectory + mapSource + '.' + this.settings.fileExtension, function(json) {
+          window.Map.mapData = json;
+          window.Map.drawMap(targetElem);
+          if (callback) {
+            return callback();
+          }
+        });
+      }
+    };
+
+    Map.drawTile = function(layer, srcTile, targetTile) {
+      var heightCount, i, index, offset, property, setData, setHeight, setWidth, target, tileset, widthCount, _i, _j, _len, _ref;
+      target = $("#tile" + layer + "-" + targetTile);
+      setData = false;
+      _ref = this.mapData.tilesets;
+      for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
+        tileset = _ref[index];
+        if (srcTile >= tileset.firstgid) {
+          setData = tileset;
+        } else {
+          return false;
+        }
+      }
+      setWidth = Math.floor(setData.imagewidth / setData.tilewidth) - 1;
+      setHeight = Math.floor(setData.imageheight / setData.tileheight) - 1;
+      heightCount = 0;
+      widthCount = 0;
+      for (i = _j = 1; _j < srcTile; i = _j += 1) {
+        if (widthCount < setWidth) {
+          widthCount++;
+        } else {
+          widthCount = 0;
+          heightCount++;
+        }
+      }
+      offset = {
+        x: (widthCount * setData.tilewidth) + (setData.spacing * widthCount) + setData.margin,
+        y: (heightCount * setData.tileheight) + (setData.spacing * heightCount) + setData.margin
+      };
+      if (setData.image) {
+        this.renderedTiles[srcTile] = "#tile" + layer + "-" + targetTile;
+        property = this.tileProperty(srcTile);
+        if (property !== false) {
+          this.tileProperties[targetTile] = property;
+        }
+        $(target).css('background-image', "url(" + setData.image + ")");
+        return $(target).css('background-position', "-" + offset.x + "px -" + offset.y + "px");
+      }
+    };
+
+    Map.setFocus = function(tileId, duration) {
+      var curloc, loc, maxheight, maxwidth;
+      loc = this.tileIdConvert(tileId);
+      curloc = $('.layer:first').position();
+      maxwidth = (Map.mapData.width * 32) - $('.viewport').width();
+      maxheight = (Map.mapData.height * 32) - $('.viewport').height();
+      loc[0] = (loc[0] * 32) - ($('.viewport').width() / 2);
+      loc[1] = (loc[1] * 32) - ($('.viewport').height() / 2);
+      if (loc[1] < 0) {
+        loc[1] = 0;
+      }
+      if (loc[0] < 0) {
+        loc[0] = 0;
+      }
+      if (loc[1] > maxheight) {
+        loc[1] = maxheight;
+      }
+      if (loc[0] > maxwidth) {
+        loc[0] = maxwidth;
+      }
+      this.isMoving = true;
+      $('.layer-container').stop();
+      return $('.layer-container').animate({
+        top: "-" + loc[1] + "px",
+        left: "-" + loc[0] + "px"
+      }, duration, function() {
+        return this.isMoving = false;
+      });
+    };
+
+    Map.tileIdConvert = function(tileInput) {
+      var i, tileId, width, x, y, _i;
+      if (typeof tileInput === 'object') {
+        tileId = tileInput[1] * this.mapData.width - 1;
+        tileId += tileInput[0];
+        return tileId;
+      } else {
+        y = 0;
+        x = 0;
+        width = this.mapData.width - 1;
+        for (i = _i = 0; _i < tileInput; i = _i += 1) {
+          if (x < width) {
+            x++;
+          } else {
+            x = 0;
+            y++;
+          }
+        }
+        return [x, y];
+      }
+    };
+
+    Map.tileIdPosition = function(tileInput) {
+      if (typeof tileInput !== 'object') {
+        tileInput = this.tileIdConvert(tileInput);
+      }
+      return [(tileInput[0] - 1) * this.mapData.tilewidth, tileInput[1] * this.mapData.tileheight];
+    };
+
+    Map.tileProperty = function(tileId) {
+      var data, index, property, _i, _len, _ref;
+      tileId = tileId - 1;
+      property = false;
+      _ref = this.mapData.tilesets;
+      for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
+        data = _ref[index];
+        if (data.tileproperties && data.tileproperties[tileId]) {
+          property = data.tileproperties[tileId].property;
+        }
+      }
+      return property;
+    };
+
+    Map.tileClick = function(e) {
+      var index, path, paths, tileId, _i, _len;
+      tileId = $(this).attr('id');
+      tileId = tileId.substr(tileId.lastIndexOf('-') + 1);
+      tileId++;
+      if (e.button === 2) {
+        return alert();
+      } else {
+        paths = window.Map.makePath(tileId);
+        if (paths.length) {
+          if (window.Map.showPaths) {
+            $("#layer" + (Map.playerLayer - 1) + " div").css('background-color', 'transparent');
+            for (index = _i = 0, _len = paths.length; _i < _len; index = ++_i) {
+              path = paths[index];
+              tileId = window.Map.tileIdConvert([path.x, path.y]);
+              $("#tile" + (Map.playerLayer - 1) + "-" + tileId).css('background-color', 'red');
+            }
+          }
+          window.Map.setFocus(tileId, paths.length * 500);
+          window.Map.playerTile = tileId;
+          return window.Character.playerMove(paths);
+        }
+      }
+    };
+
+    Map.makePath = function(toTileId) {
+      var board, fromTileLoc, prop, tile, tileidc, toTileLoc, totalMapSize, x, y, _i, _j, _ref, _ref1;
+      totalMapSize = this.mapData.width * this.mapData.height;
+      toTileLoc = this.tileIdConvert(toTileId);
+      fromTileLoc = this.tileIdConvert(Map.playerTile);
+      board = [];
+      for (y = _i = 0, _ref = Map.mapData.height - 1; _i < _ref; y = _i += 1) {
+        board[y] = [];
+        for (x = _j = 0, _ref1 = Map.mapData.width - 1; _j <= _ref1; x = _j += 1) {
+          tile = this.tileIdConvert([x, y]);
+          prop = this.tileProperties[tile];
+          if (prop === 'block') {
+            board[y][x] = 1;
+            if (this.showBlocked) {
+              tileidc = this.tileIdConvert([x, y]);
+              $("#tile0-" + tileidc).css('background', 'red');
+            }
+          } else {
+            board[y][x] = 0;
+          }
+        }
+      }
+      return AStar(board, fromTileLoc, toTileLoc, 'Diagonal');
+    };
+
+    return Map;
+
+  })();
+
+}).call(this);
